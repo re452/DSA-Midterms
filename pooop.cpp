d9b11774c267a1b6797f25c011f26fea43a1ca03
re452 <jhrndz321@gmail.com>
2025-04-06 18:41:49 +0800

    Update pooop.cpp

--- pooop.cpp
+++ pooop.cpp
@@ -1,13 +1,1 @@
-    while (!s_num.empty() && !q_num.empty()) {
-        combined_v.push_back(s_num.top());
-        s_num.pop();
-        combined_v.push_back(q_num.front());
-        q_num.pop();
-    }
-    cout << "Two: ";
-    for (int i = 0; i < combined_v.size(); i++) {
-        cout << combined_v[i] << " ";
-    }
-    cout << endl;
-    return 0;
-}
+  // how do you delete this help
